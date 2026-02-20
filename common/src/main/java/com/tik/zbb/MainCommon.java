23e007c8e1599b9425a0388d9242a02cefcd0861
XTiK555 <rostislav.001500@gmail.com>
2026-02-20 19:41:13 +0700

    Removed attackAllEntities option

--- common/src/main/java/com/tik/zbb/MainCommon.java
+++ common/src/main/java/com/tik/zbb/MainCommon.java
@@ -3,11 +3,9 @@ package com.tik.zbb;
 import com.tik.zbb.config.ConfigData;
 import com.tik.zbb.config.ConfigManager;
 import com.tik.zbb.goals.BreakAndBuildGoal;
-import com.tik.zbb.goals.ThroughWallsNearestTargetGoal;
 import com.tik.zbb.mixin.accessor.MobAccessor;
 import com.tik.zbb.utilities.SecondsToTicksUtility;
 import com.tik.zbb.utilities.ShouldApplyToMobUtility;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
@@ -53,11 +51,7 @@ public class MainCommon
         PathfinderMob pFMob = (PathfinderMob) mob;
 
         pFMobAccessor.zbb$getGoalSelector().addGoal(2, new BreakAndBuildGoal(pFMob));
-
-        if (config.attackAllEntities)
-        {
-            pFMobAccessor.zbb$getTargetSelector().addGoal(1, new ThroughWallsNearestTargetGoal<>(pFMob, LivingEntity.class));
-        }
+        
         if (followRangeAttribute != null && followRangeAttribute.getBaseValue() < config.targetSearchRadius)
         {
             followRangeAttribute.setBaseValue(config.targetSearchRadius);
