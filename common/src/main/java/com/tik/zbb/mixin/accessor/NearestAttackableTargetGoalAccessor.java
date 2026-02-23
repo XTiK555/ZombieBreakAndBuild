84ccd6806c513eb1ff51c847606c9da9487b5701
XTiK555 <rostislav.001500@gmail.com>
2026-02-23 15:18:04 +0700

    Switched back to goals instead of mixins
    for alwaysSeeNearestPlayer

--- common/src/main/java/com/tik/zbb/mixin/accessor/NearestAttackableTargetGoalAccessor.java
+++ common/src/main/java/com/tik/zbb/mixin/accessor/NearestAttackableTargetGoalAccessor.java
@@ -1,13 +1,9 @@
 package com.tik.zbb.mixin.accessor;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
-
-import javax.annotation.Nullable;
 
 @Mixin(NearestAttackableTargetGoal.class)
 public interface NearestAttackableTargetGoalAccessor
@@ -17,7 +13,4 @@ public interface NearestAttackableTargetGoalAccessor
 
     @Accessor("targetConditions")
     TargetingConditions zbb$getTargetConditions();
-
-    @Invoker("setTarget")
-    void zbb$setTarget(@Nullable LivingEntity target);
 }
