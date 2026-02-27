08386db951aae31cd5f2eb90c4cbd309ffa2b7a5
XTiK555 <rostislav.001500@gmail.com>
2026-02-27 21:37:55 +0700

    Fixed mob positions in tactics

--- common/src/main/java/com/tik/zbb/ai/state/tactic/tactics/AdjustHeightToTargetTactic.java
+++ common/src/main/java/com/tik/zbb/ai/state/tactic/tactics/AdjustHeightToTargetTactic.java
@@ -5,6 +5,7 @@ import com.tik.zbb.ai.state.tactic.IMobTactic;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.Block;
 
 import static com.tik.zbb.utilities.IsFreePassUtility.isFreePass;
@@ -20,17 +21,22 @@ public class AdjustHeightToTargetTactic implements IMobTactic
     {
         if (blockRegistry == null) blockRegistry = context.getLevel().registryAccess().lookupOrThrow(Registries.BLOCK);
 
-        if (context.getTarget().getY() > context.getMob().getY() + 1)
+        int mobX = Mth.floor(context.getMob().getX());
+        int mobY = Mth.floor(context.getMob().getY());
+        int mobZ = Mth.floor(context.getMob().getZ());
+        int targetY = Mth.floor(context.getTarget().getY());
+
+        if (targetY > mobY + 1)
         {
-            if (!isFreePass(tmpBlockPos.set(context.getMob().getX(), context.getMob().getY() + 1, context.getMob().getZ()), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
+            if (!isFreePass(tmpBlockPos.set(mobX, mobY + 1, mobZ), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
                 context.getActionExecutor().tryExecuteBreakAction(tmpBlockPos);
-            if (!isFreePass(tmpBlockPos.set(context.getMob().getX(), context.getMob().getY() + 2, context.getMob().getZ()), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
+            if (!isFreePass(tmpBlockPos.set(mobX, mobY + 2, mobZ), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
                 context.getActionExecutor().tryExecuteBreakAction(tmpBlockPos);
 
             // If there is space above, we try to adjust and jump.
-            if (isFreePass(tmpBlockPos.set(context.getMob().getX(), context.getMob().getY() + 2, context.getMob().getZ()), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
+            if (isFreePass(tmpBlockPos.set(mobX, mobY + 2, mobZ), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
             {
-                if (context.getActionExecutor().tryExecuteBuildAction(tmpBlockPos.set(context.getMob().getX(), context.getMob().getY(), context.getMob().getZ())))
+                if (context.getActionExecutor().tryExecuteBuildAction(tmpBlockPos.set(mobX, mobY, mobZ)))
                 {
                     context.getMob().getJumpControl().jump();
                 }
@@ -39,9 +45,9 @@ public class AdjustHeightToTargetTactic implements IMobTactic
         }
 
         // target below -> break the block below you (if it's preventing you from getting down)
-        if (context.getTarget().getY() < context.getMob().getY() - 1)
+        if (targetY < mobY - 1)
         {
-            if (!isFreePass(tmpBlockPos.set(context.getMob().getX(), context.getMob().getY() - 1, context.getMob().getZ()), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
+            if (!isFreePass(tmpBlockPos.set(mobX, mobY - 1, mobZ), blockRegistry, context.getLevel(), context.getConfigSnapshot().data()))
             {
                 context.getActionExecutor().tryExecuteBreakAction(tmpBlockPos);
             }
