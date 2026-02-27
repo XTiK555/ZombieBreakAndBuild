4b1dc55344f3078853748dc84852325efd8d52c2
XTiK555 <rostislav.001500@gmail.com>
2026-02-27 11:07:37 +0700

    Renamed mixins
    fixed seeTargetsThroughWalls

--- common/src/main/java/com/tik/zbb/mixin/accessor/NATGAccessor.java
+++ common/src/main/java/com/tik/zbb/mixin/accessor/NATGAccessor.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(NearestAttackableTargetGoal.class)
-public interface NearestAttackableTargetGoalAccessor
+public interface NATGAccessor
 {
     @Accessor("targetType")
     Class<?> zbb$getTargetType();
