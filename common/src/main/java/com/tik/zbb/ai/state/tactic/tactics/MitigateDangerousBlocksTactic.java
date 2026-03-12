0a523065b10536042f567a9d15eab05ffb6633f5
XTiK555 <rostislav.001500@gmail.com>
2026-03-12 16:12:41 +0700

    Zombies now trying to break dangerous blocks

--- common/src/main/java/com/tik/zbb/ai/state/tactic/tactics/MitigateDangerousBlocksTactic.java
+++ common/src/main/java/com/tik/zbb/ai/state/tactic/tactics/MitigateDangerousBlocksTactic.java
@@ -46,7 +46,10 @@ public class MitigateDangerousBlocksTactic implements IMobTactic
 
                     if (!context.getActionExecutor().tryExecuteBuildAction(tmpBlockPos))
                     {
-                        context.getActionExecutor().tryExecuteBuildAction(blockCover);
+                        if (!context.getActionExecutor().tryExecuteBreakAction(tmpBlockPos))
+                        {
+                            context.getActionExecutor().tryExecuteBuildAction(blockCover);
+                        }
                     }
                 }
             }
