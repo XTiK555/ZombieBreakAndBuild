4c0658c9be4beadebb82e0bba43136d31fd11189
XTiK555 <rostislav.001500@gmail.com>
2026-03-19 11:36:04 +0700

    Removed additionalZombieEquipmentSpawnChance

--- common/src/main/java/com/tik/zbb/config/ConfigData.java
+++ common/src/main/java/com/tik/zbb/config/ConfigData.java
@@ -122,10 +122,6 @@ public class ConfigData
         @Comment("Distance to path end before breaking or building")
         public int endNodeBreakBuildDistance = 6;
 
-        @Range(min = 0, max = 100)
-        @Comment("Additional zombie equipment spawn chance. 0 - vanilla, 100 - 100% chance")
-        public int additionalZombieEquipmentSpawnChance = 10;
-
         public BlockDamage blockDamage = new BlockDamage();
 
         public Cooldowns cooldowns = new Cooldowns();
