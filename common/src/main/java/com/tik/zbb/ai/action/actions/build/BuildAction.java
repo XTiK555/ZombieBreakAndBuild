5a5634a09c50d69ce24f535a3fce110b92e8b996
XTiK555 <rostislav.001500@gmail.com>
2026-03-23 15:13:39 +0700

    Built block disappear now restores old block state

--- common/src/main/java/com/tik/zbb/ai/action/actions/build/BuildAction.java
+++ common/src/main/java/com/tik/zbb/ai/action/actions/build/BuildAction.java
@@ -7,14 +7,12 @@ import com.tik.zbb.config.ConfigSnapshot;
 import com.tik.zbb.utilities.SecondsToTicksUtility;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class BuildAction implements IMobAction<BuildRequest>
 {
     public record OnAnyBlockPlacedEvent(ServerLevel level, BlockPos pos, ConfigSnapshot configSnapshot,
-                                        BlockState state) {}
+                                        BlockState placedState, BlockState oldState) {}
 
     @Override
     public boolean canExecute(MobActionContext context, BuildRequest request)
@@ -31,11 +29,12 @@ public class BuildAction implements IMobAction<BuildRequest>
     @Override
     public void execute(MobActionContext context, BuildRequest request)
     {
+        BlockState oldState = context.level().getBlockState(request.pos());
         BlockState placedState = request.bridgeBlock().defaultBlockState();
 
         if (context.level().setBlockAndUpdate(request.pos(), request.bridgeBlock().defaultBlockState()))
         {
-            Constants.EVENT_BUS.post(new OnAnyBlockPlacedEvent(context.level(), request.pos(), context.configSnapshot(), placedState));
+            Constants.EVENT_BUS.post(new OnAnyBlockPlacedEvent(context.level(), request.pos(), context.configSnapshot(), placedState, oldState));
         }
 
         context.aiTimers().setBuildCooldownUntil(context.level().getGameTime() + SecondsToTicksUtility.toTicks(context.configSnapshot().data().balance.cooldowns.buildCooldown, 1));
