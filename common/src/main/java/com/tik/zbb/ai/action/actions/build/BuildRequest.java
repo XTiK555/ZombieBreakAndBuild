261615657491626065dbe6354535264cb49a2552
XTiK555 <rostislav.001500@gmail.com>
2026-04-11 14:23:54 +0700

    Use immutable BlockPos in break/build actions. Fixes #1

--- common/src/main/java/com/tik/zbb/ai/action/actions/build/BuildRequest.java
+++ common/src/main/java/com/tik/zbb/ai/action/actions/build/BuildRequest.java
@@ -3,4 +3,10 @@ package com.tik.zbb.ai.action.actions.build;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
 
-public record BuildRequest(BlockPos pos, Block bridgeBlock) {}
+public record BuildRequest(BlockPos pos, Block bridgeBlock)
+{
+    public BuildRequest
+    {
+        pos = pos.immutable();
+    }
+}
