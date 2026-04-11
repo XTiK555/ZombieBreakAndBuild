035f6300c250c99f8eb00f7b17c4131469658225
XTiK555 <rostislav.001500@gmail.com>
2026-04-11 16:24:18 +0700

    Small fixes

--- neoforge/src/main/java/com/tik/zbb/MainNeoForge.java
+++ neoforge/src/main/java/com/tik/zbb/MainNeoForge.java
@@ -44,7 +44,7 @@ public class MainNeoForge
     }
 
     @SubscribeEvent
-    public void onServerTick(ServerStoppingEvent event)
+    public void onServerStopping(ServerStoppingEvent event)
     {
         MainCommon.onServerStopping(event.getServer());
     }
