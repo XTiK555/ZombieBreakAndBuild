9c2f5b7775f6ecdc9cd440e3108e2a7d12c5934c
XTiK555 <rostislav.001500@gmail.com>
2026-05-08 15:25:26 +0700

    Visual code in DamageBlckStorageManager moved to separate visual script.

--- common/src/main/java/com/tik/zbb/event/EventRegistrar.java
+++ common/src/main/java/com/tik/zbb/event/EventRegistrar.java
@@ -6,6 +6,7 @@ import com.tik.zbb.ai.action.actions.build.BuildVisual;
 import com.tik.zbb.blockstorage.BlockStorages;
 import com.tik.zbb.blockstorage.storages.broken.BrokenReappearBlockVisual;
 import com.tik.zbb.blockstorage.storages.buildDisappear.BuildDisappearBlockVisual;
+import com.tik.zbb.blockstorage.storages.damage.DamageBlockVisual;
 
 public final class EventRegistrar
 {
@@ -21,6 +22,7 @@ public final class EventRegistrar
         // storage visuals
         Constants.EVENT_BUS.register(new BrokenReappearBlockVisual());
         Constants.EVENT_BUS.register(new BuildDisappearBlockVisual());
+        Constants.EVENT_BUS.register(new DamageBlockVisual());
 
 
         // action visuals
