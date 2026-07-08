ab759e866ec8ac1680b1e65f286156ab757a4de4
XTiK555 <rostislav.001500@gmail.com>
2026-07-08 12:08:29 +0700

    Add continueSeeingTargetsThroughBlocksLimit setting

--- common/src/main/java/com/tik/zbb/mixin/NATGoalMixin.java
+++ common/src/main/java/com/tik/zbb/mixin/NATGoalMixin.java
@@ -4,17 +4,13 @@ import com.tik.zbb.config.ConfigData;
 import com.tik.zbb.config.ConfigManager;
 import com.tik.zbb.mixin.accessor.TargetingConditionsAccessor;
 import com.tik.zbb.utilities.ShouldApplyToMobUtility;
-import net.minecraft.core.BlockPos;
+import com.tik.zbb.utilities.TargetVisibilityThroughBlocksUtility;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.TargetGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -46,7 +42,7 @@ public abstract class NATGoalMixin extends TargetGoal
 
         TargetingConditions.Selector combinedSelector = (candidate, level) ->
                 (oldSelector == null || oldSelector.test(candidate, level)) &&
-                        zbb$canNoticeTargetThroughSolidBlocks(
+                        TargetVisibilityThroughBlocksUtility.canSeeThroughSolidBlocks(
                                 this.mob,
                                 candidate,
                                 data.ai.noticeTargetsThroughBlocksLimit
@@ -56,61 +52,5 @@ public abstract class NATGoalMixin extends TargetGoal
                 .ignoreLineOfSight()
                 .selector(combinedSelector)
         );
-    }
-
-    @Unique
-    private static boolean zbb$canNoticeTargetThroughSolidBlocks(Mob mob, LivingEntity target, int maxSolidBlocks)
-    {
-        if (maxSolidBlocks == 0)
-        {
-            return true;
-        }
-
-        Level level = mob.level();
-        Vec3 from = mob.getEyePosition();
-        Vec3 to = target.getEyePosition();
-
-        double distance = from.distanceTo(to);
-        if (distance <= 0.0001D)
-        {
-            return true;
-        }
-
-        int steps = Math.max(1, (int) Math.ceil(distance * 5.0D));
-        int solidBlocks = 0;
-
-        BlockPos startPos = BlockPos.containing(from);
-        BlockPos endPos = BlockPos.containing(to);
-        BlockPos lastPos = null;
-
-        for (int i = 1; i < steps; i++)
-        {
-            double t = (double) i / (double) steps;
-            Vec3 point = from.lerp(to, t);
-            BlockPos pos = BlockPos.containing(point);
-
-            if (pos.equals(lastPos))
-            {
-                continue;
-            }
-            lastPos = pos;
-
-            if (pos.equals(startPos) || pos.equals(endPos))
-            {
-                continue;
-            }
-
-            BlockState state = level.getBlockState(pos);
-            if (!state.isAir() && state.isSolidRender())
-            {
-                solidBlocks++;
-                if (solidBlocks > maxSolidBlocks)
-                {
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 }
