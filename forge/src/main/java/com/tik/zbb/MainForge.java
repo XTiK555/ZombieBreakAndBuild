5e49bc4f73e87f0297fa2574a7276b95b3380486
XTiK555 <rostislav.001500@gmail.com>
2026-07-08 14:44:25 +0700

    Moved config reload to MainCommon.onReload()

--- forge/src/main/java/com/tik/zbb/MainForge.java
+++ forge/src/main/java/com/tik/zbb/MainForge.java
@@ -1,6 +1,5 @@
 package com.tik.zbb;
 
-import com.tik.zbb.config.ConfigManager;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.entity.Mob;
@@ -56,6 +55,6 @@ public class MainForge
     @SubscribeEvent
     public void onAddReloadListeners(AddReloadListenerEvent event)
     {
-        event.addListener((ResourceManagerReloadListener) resourceManager -> ConfigManager.reload());
+        event.addListener((ResourceManagerReloadListener) resourceManager -> MainCommon.onReload());
     }
 }
