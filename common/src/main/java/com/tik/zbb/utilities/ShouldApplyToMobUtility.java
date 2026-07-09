263b58c150565fcae0a40914ebc3efad8d5cfce4
XTiK555 <rostislav.001500@gmail.com>
2026-07-09 21:42:03 +0700

    Add ConfigRuntime

--- common/src/main/java/com/tik/zbb/utilities/ShouldApplyToMobUtility.java
+++ common/src/main/java/com/tik/zbb/utilities/ShouldApplyToMobUtility.java
@@ -1,6 +1,7 @@
 package com.tik.zbb.utilities;
 
 import com.tik.zbb.config.ConfigData;
+import com.tik.zbb.config.ConfigSnapshot;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
@@ -16,16 +17,18 @@ import net.minecraft.world.entity.monster.zombie.ZombieVillager;
 
 public final class ShouldApplyToMobUtility
 {
-    public static boolean matchesZbbMobFilter(Mob mob, ConfigData config)
+    public static boolean matchesZbbMobFilter(Mob mob, ConfigSnapshot configSnapshot)
     {
+        ConfigData config = configSnapshot.data();
+
         if (!(mob.level() instanceof ServerLevel)) return false;
         if (!(mob instanceof PathfinderMob)) return false;
 
         Registry<EntityType> entityTypeRegistry = mob.level().registryAccess().lookupOrThrow(Registries.ENTITY_TYPE);
         Identifier entityId = entityTypeRegistry.getKey(mob.getType());
 
-        if (entityId != null && config.additionalEntityIdSet.contains(entityId)) return true;
-        if (entityId != null && config.ignoreBuildEntityIdSet.contains(entityId) && config.ignoreBreakEntityIdSet.contains(entityId))
+        if (entityId != null && configSnapshot.runtime().additionalEntityIdSet().contains(entityId)) return true;
+        if (entityId != null && configSnapshot.runtime().ignoreBuildEntityIdSet().contains(entityId) && configSnapshot.runtime().ignoreBreakEntityIdSet().contains(entityId))
             return false;
         if (mob.getType().getCategory() != MobCategory.MONSTER) return false;
         if (config.ai.applyToAllMonsters) return true;
