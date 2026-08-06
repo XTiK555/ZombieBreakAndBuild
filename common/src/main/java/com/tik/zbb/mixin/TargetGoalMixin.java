0fff08053cbd443c09c9a93de6f3deb8d54b01b8
XTiK555 <rostislav.001500@gmail.com>
2026-08-06 13:56:34 +0700

    Switch TargetGoalMixin to WrapOperation

--- common/src/main/java/com/tik/zbb/mixin/TargetGoalMixin.java
+++ common/src/main/java/com/tik/zbb/mixin/TargetGoalMixin.java
@@ -4,6 +4,8 @@ import com.tik.zbb.config.ConfigManager;
 import com.tik.zbb.config.ConfigSnapshot;
 import com.tik.zbb.utilities.ShouldApplyToMobUtility;
 import com.tik.zbb.utilities.TargetVisibilityThroughBlocksUtility;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
@@ -13,7 +15,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TargetGoal.class)
 public abstract class TargetGoalMixin
@@ -22,10 +23,10 @@ public abstract class TargetGoalMixin
     @Final
     protected Mob mob;
 
-    @Redirect(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/sensing/Sensing;hasLineOfSight(Lnet/minecraft/world/entity/Entity;)Z"))
-    private boolean zbb$hasLineOfSight(Sensing sensing, Entity target)
+    @WrapOperation(method = "canContinueToUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/sensing/Sensing;hasLineOfSight(Lnet/minecraft/world/entity/Entity;)Z"))
+    private boolean zbb$hasLineOfSight(Sensing sensing, Entity target, Operation<Boolean> original)
     {
-        if (sensing.hasLineOfSight(target)) return true;
+        if (original.call(sensing, target)) return true;
 
         ConfigSnapshot configSnapshot = ConfigManager.getConfigSnapshot();
         if (configSnapshot.game().ai().canContinueSeeingTargetsThroughBlocks()
