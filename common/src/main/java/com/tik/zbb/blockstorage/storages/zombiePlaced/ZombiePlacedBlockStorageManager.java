0cc04c29452856d83b7f32bdd9857471f19f8fb9
XTiK555 <rostislav.001500@gmail.com>
2026-08-06 12:35:14 +0700

    Add temp optimization in ZombieBlockStorage

--- common/src/main/java/com/tik/zbb/blockstorage/storages/zombiePlaced/ZombiePlacedBlockStorageManager.java
+++ common/src/main/java/com/tik/zbb/blockstorage/storages/zombiePlaced/ZombiePlacedBlockStorageManager.java
@@ -1,6 +1,7 @@
 package com.tik.zbb.blockstorage.storages.zombiePlaced;
 
 import com.tik.zbb.ai.action.actions.build.BuildAction;
+import com.tik.zbb.config.ConfigGame;
 import com.tik.zbb.event.MixinEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -13,6 +14,8 @@ public class ZombiePlacedBlockStorageManager
     @Subscribe
     public void onAnyBlockPlaced(BuildAction.OnAnyBlockPlacedEvent event)
     {
+        if (!storageConditions(event.configSnapshot().game())) return;
+
         storage.put(event.level(), event.pos(), event.placedState());
     }
 
@@ -30,5 +33,14 @@ public class ZombiePlacedBlockStorageManager
     public void remove(ServerLevel level, BlockPos pos)
     {
         storage.remove(level, pos);
+    }
+
+    // temp for optimization, may be removed in the future
+    private boolean storageConditions(ConfigGame config)
+    {
+        boolean brokenRestoring = config.blockRestoration().brokenBlocksRestoring();
+        boolean builtDisappear = config.blockRestoration().builtBlocksDisappearing();
+
+        return brokenRestoring && !builtDisappear;
     }
 }
