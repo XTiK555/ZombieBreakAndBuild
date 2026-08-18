5c6b935e67cc9086a0e9bfa3e197a23e3968d58f
XTiK555 <rostislav.001500@gmail.com>
2026-08-18 22:43:38 +0700

    refactor(storage): move entry codecs into entry records

--- common/src/main/java/com/tik/zbb/blockstorage/storages/buildDisappear/BuildDisappearBlockStorageEntry.java
+++ common/src/main/java/com/tik/zbb/blockstorage/storages/buildDisappear/BuildDisappearBlockStorageEntry.java
@@ -1,6 +1,29 @@
 package com.tik.zbb.blockstorage.storages.buildDisappear;
 
-import net.minecraft.world.level.block.state.BlockState;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.state.BlockState;
 
-public record BuildDisappearBlockStorageEntry(BlockState placedState, BlockState oldState, CompoundTag oldNbt) {}
+import java.util.Optional;
+
+public record BuildDisappearBlockStorageEntry(BlockState placedState, BlockState oldState, CompoundTag oldNbt)
+{
+    static final Codec<BuildDisappearBlockStorageEntry> CODEC =
+            RecordCodecBuilder.create(instance -> instance.group(
+                    BlockState.CODEC
+                            .fieldOf("placed_state")
+                            .forGetter(BuildDisappearBlockStorageEntry::placedState),
+                    BlockState.CODEC
+                            .fieldOf("old_state")
+                            .forGetter(BuildDisappearBlockStorageEntry::oldState),
+                    CompoundTag.CODEC.
+                            optionalFieldOf("old_nbt")
+                            .forGetter(entry -> Optional.ofNullable(entry.oldNbt()))
+            ).apply(instance, (placedState, oldState, oldNbt) ->
+                    new BuildDisappearBlockStorageEntry(
+                            placedState,
+                            oldState,
+                            oldNbt.orElse(null)
+                    )));
+}
