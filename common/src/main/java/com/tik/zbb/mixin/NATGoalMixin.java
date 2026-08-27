c6d85cb8c6d39334ab04adc8673cd42fed2b5977
XTiK555 <rostislav.001500@gmail.com>
2026-08-27 18:41:08 +0700

    refactor(mixins): improve injection points and reduce coupling

--- common/src/main/java/com/tik/zbb/mixin/NATGoalMixin.java
+++ common/src/main/java/com/tik/zbb/mixin/NATGoalMixin.java
@@ -1,5 +1,6 @@
 package com.tik.zbb.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.tik.zbb.config.ConfigManager;
 import com.tik.zbb.config.ConfigSnapshot;
 import com.tik.zbb.mixin.accessor.TargetingConditionsAccessor;
@@ -11,8 +12,6 @@ import net.minecraft.world.entity.ai.goal.target.TargetGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NearestAttackableTargetGoal.class)
 public abstract class NATGoalMixin extends TargetGoal
@@ -22,15 +21,14 @@ public abstract class NATGoalMixin extends TargetGoal
         super(mob, mustSee);
     }
 
-    @Inject(method = "getTargetConditions", at = @At("RETURN"), cancellable = true)
-    private void zbb$getTargetConditions(CallbackInfoReturnable<TargetingConditions> cir)
+    @ModifyReturnValue(method = "getTargetConditions", at = @At("RETURN"))
+    private TargetingConditions zbb$getTargetConditions(TargetingConditions original)
     {
         ConfigSnapshot configSnapshot = ConfigManager.getConfigSnapshot();
 
-        if (!configSnapshot.game().ai().canNoticeTargetsThroughBlocks()) return;
-        if (!ShouldApplyToMobUtility.matchesFullZbbMobFilter(this.mob, configSnapshot)) return;
+        if (!configSnapshot.game().ai().canNoticeTargetsThroughBlocks()) return original;
+        if (!ShouldApplyToMobUtility.matchesFullZbbMobFilter(this.mob, configSnapshot)) return original;
 
-        TargetingConditions original = cir.getReturnValue();
         TargetingConditions.Selector oldSelector = ((TargetingConditionsAccessor) (Object) original).zbb$getSelector();
 
         TargetingConditions.Selector combinedSelector = (candidate, level) ->
@@ -41,9 +39,8 @@ public abstract class NATGoalMixin extends TargetGoal
                                 configSnapshot.game().ai().noticeTargetsThroughBlocksLimit()
                         );
 
-        cir.setReturnValue(original.copy()
+        return original.copy()
                 .ignoreLineOfSight()
-                .selector(combinedSelector)
-        );
+                .selector(combinedSelector);
     }
 }
