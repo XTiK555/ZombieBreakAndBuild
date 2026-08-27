bc78d4b37b52263980dbcd4bdc102c8d6c5a2904
XTiK555 <rostislav.001500@gmail.com>
2026-08-27 18:41:08 +0700

    refactor(mixins): improve injection points and reduce coupling

--- common/src/main/java/com/tik/zbb/mixin/NATGoalMixin.java
+++ common/src/main/java/com/tik/zbb/mixin/NATGoalMixin.java
@@ -1,5 +1,6 @@
 package com.tik.zbb.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.tik.zbb.config.ConfigManager;
 import com.tik.zbb.config.ConfigSnapshot;
 import com.tik.zbb.mixin.accessor.TargetingConditionsAccessor;
@@ -11,58 +12,41 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.TargetGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Predicate;
 
 @Mixin(NearestAttackableTargetGoal.class)
 public abstract class NATGoalMixin extends TargetGoal
 {
-    @Shadow
-    protected TargetingConditions targetConditions;
-
-    @Unique
-    private TargetingConditions zbb$originalTargetConditions;
-
     protected NATGoalMixin(Mob mob, boolean mustSee)
     {
         super(mob, mustSee);
     }
 
-    @Inject(method = "canUse", at = @At("HEAD"))
-    private void zbb$adjustLineOfSightBeforeCanUse(CallbackInfoReturnable<Boolean> cir)
+    @ModifyExpressionValue(method = "findTarget", at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/world/entity/ai/goal/target/NearestAttackableTargetGoal;targetConditions:Lnet/minecraft/world/entity/ai/targeting/TargetingConditions;"
+    ))
+    private TargetingConditions zbb$getTargetConditions(TargetingConditions original)
     {
-        if (this.zbb$originalTargetConditions == null)
-        {
-            this.zbb$originalTargetConditions = this.targetConditions;
-        }
-
         ConfigSnapshot configSnapshot = ConfigManager.getConfigSnapshot();
 
-        if (configSnapshot.game().ai().canNoticeTargetsThroughBlocks() && ShouldApplyToMobUtility.matchesFullZbbMobFilter(this.mob, configSnapshot))
-        {
-            Predicate<LivingEntity> oldSelector =
-                    ((TargetingConditionsAccessor) (Object) this.zbb$originalTargetConditions).zbb$getSelector();
+        if (!configSnapshot.game().ai().canNoticeTargetsThroughBlocks()) return original;
+        if (!ShouldApplyToMobUtility.matchesFullZbbMobFilter(this.mob, configSnapshot)) return original;
 
-            Predicate<LivingEntity> combinedSelector = (candidate) ->
-                    (oldSelector == null || oldSelector.test(candidate)) &&
-                            TargetVisibilityThroughBlocksUtility.canSeeThroughSolidBlocks(
-                                    this.mob,
-                                    candidate,
-                                    configSnapshot.game().ai().noticeTargetsThroughBlocksLimit()
-                            );
+        Predicate<LivingEntity> oldSelector = ((TargetingConditionsAccessor) (Object) original).zbb$getSelector();
 
-            this.targetConditions = this.zbb$originalTargetConditions.copy()
-                    .ignoreLineOfSight()
-                    .selector(combinedSelector);
-        }
-        else
-        {
-            this.targetConditions = this.zbb$originalTargetConditions;
-        }
+        Predicate<LivingEntity> combinedSelector = candidate ->
+                (oldSelector == null || oldSelector.test(candidate)) &&
+                        TargetVisibilityThroughBlocksUtility.canSeeThroughSolidBlocks(
+                                this.mob,
+                                candidate,
+                                configSnapshot.game().ai().noticeTargetsThroughBlocksLimit()
+                        );
+
+        return original.copy()
+                .ignoreLineOfSight()
+                .selector(combinedSelector);
     }
 }
