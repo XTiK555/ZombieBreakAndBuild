da241dd6a280664170329f4e791d4f2323942d52
XTiK555 <rostislav.001500@gmail.com>
2026-08-27 22:38:42 +0700

    fix(block-damage): update cracks from storage changes

--- common/src/main/java/com/tik/zbb/ai/action/actions/breakk/BreakVisual.java
+++ common/src/main/java/com/tik/zbb/ai/action/actions/breakk/BreakVisual.java
@@ -1,10 +1,13 @@
 package com.tik.zbb.ai.action.actions.breakk;
 
 import com.tik.zbb.config.ConfigGame;
-import com.tik.zbb.config.ConfigManager;
+import com.tik.zbb.config.ConfigSnapshot;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.greenrobot.eventbus.Subscribe;
 
 public class BreakVisual
@@ -12,27 +15,27 @@ public class BreakVisual
     @Subscribe
     public void onAnyBlockBroken(BreakAction.OnAnyBlockBrokenEvent event)
     {
-        ConfigGame.VisualEffects visualEffects = ConfigManager.getConfigSnapshot().game().visualEffects();
-
-        if (visualEffects.breakMobSwing()) mobSwing(event.mob());
+        playEffects(event.level(), event.pos(), event.oldState(), event.mob(), event.configSnapshot());
     }
 
     @Subscribe
     public void onAnyBlockHit(BreakAction.OnAnyBlockHit event)
     {
-        ConfigGame.VisualEffects visualEffects = ConfigManager.getConfigSnapshot().game().visualEffects();
-        int stage = (int) Math.min(9L, ((long) event.storageEntry().totalDamage() * 10L) / event.blockHealth());
-
-        event.level().destroyBlockProgress(event.storageEntry().blockPosId(), event.pos(), stage);
-        event.level().levelEvent(2001, event.pos(), Block.getId(event.state())); // particles and sound
-
-        if (visualEffects.breakMobSwing()) mobSwing(event.mob());
+        playEffects(event.level(), event.pos(), event.state(), event.mob(), event.configSnapshot());
     }
 
-    private void mobSwing(PathfinderMob mob)
+    private void playEffects(ServerLevel level, BlockPos pos, BlockState blockState, PathfinderMob mob, ConfigSnapshot configSnapshot)
     {
+        level.levelEvent(2001, pos, Block.getId(blockState)); // particles and sound
+
+        mobSwing(mob, configSnapshot.game());
+    }
+
+    private void mobSwing(PathfinderMob mob, ConfigGame config)
+    {
+        if (!config.visualEffects().breakMobSwing()) return;
+
         mob.swing(InteractionHand.MAIN_HAND);
         mob.swing(InteractionHand.OFF_HAND);
     }
-
 }
